ab74ec14a3c2899cc03b5a373b56bdb0f90d563d
MrBaoquan <mrma617@gmail.com>
2024-10-22 10:53:49 +0800

    添加示例

---
+++ Samples~/Basic Usage/UNIPlayerSampleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UNIHper;
+using UniRx;
+
+public class UNIPlayerSampleScript : SceneScriptBase
+{
+    // Called once after scene is loaded
+    private void Start() { }
+
+    // Called per frame after Start
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Managements.Config.Get<UNIPlayer>().PlayVideo("v1");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Managements.Config.Get<UNIPlayer>().PlayVideo("v2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Managements.Config.Get<UNIPlayer>().Back2Idle();
+        }
+    }
+
+    // Called when scene is unloaded
+    private void OnDestroy() { }
+
+    // Called when application is quit
+    private void OnApplicationQuit() { }
+}
